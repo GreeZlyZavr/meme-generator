276e16a0e0fc9ac4d043b5971d153eb5f6529b68
Timofei <gryazev.tim@yandex.ru>
2023-05-22 14:44:31 +0300

    First commit

---
+++ src/components/MemeGenerator.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+class MemeGenerator extends React.Component {
+    constructor() {
+        super();
+        this.state ={
+
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>MEME GENERATOR SECTION</h1>
+            </div>
+        );
+    }
+
+};
+
+export default MemeGenerator;
